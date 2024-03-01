c59c5215611e16365ee6a2a773708d779e24faba
C-ITO <C-ITO@kan-adminnoMacBook-Air.local>
2024-03-01 17:13:45 +0900

    gtaタグ

--- header.php
+++ header.php
@@ -49,6 +49,15 @@
 	<script src="https://unpkg.com/split-type"></script>
 	<?php wp_head(); ?>
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-0K84EBD48C"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0K84EBD48C');
+</script>
 
 <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
